7cf2744cc086f32a9500a60992e1980ebd74a877
patricia <patriciareiasilva@gmail.com>
2021-02-14 21:56:09 +0000

    inicial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// dependency imports
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+//relative imports
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+//Necessery to connect to dn before server
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected');
+        return server.listen({ port: 5000 });
+    })
+    .then((res) => {
+        console.log(`Server is running as ${res.url}`);
+    });
