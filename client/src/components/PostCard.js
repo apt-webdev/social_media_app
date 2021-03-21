1936734ebb3f6b4d24216c6a0f10a038ff6f38d1
patricia <patriciareiasilva@gmail.com>
2021-03-21 00:08:26 +0000

    create likeButton logic. Verify logged user for making a like. Add detele button without back logic. Pass user and post info from prosp to LikeButton component.

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -3,6 +3,9 @@ import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
+
 /* import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
 import DeleteButton from './DeleteButton';
@@ -12,13 +15,14 @@ function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes }
 }) {
   //const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext)
 
   function likePost(){
     console.log("Liked Post!");
   }
-  function commentOnPost(){
+  /* function commentOnPost(){
     console.log("Comment on post!");
-  }
+  } */
 
   return (
     <Card fluid>
@@ -35,27 +39,20 @@ function PostCard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
        <Card.Content extra>
-        {/* <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+         {/*
         <MyPopup content="Comment on post">
-          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+           <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
             <Button color="blue" basic>
               <Icon name="comments" />
             </Button>
             <Label basic color="blue" pointing="left">
               {commentCount}
             </Label>
-          </Button>
-        </MyPopup>
-        {user && user.username === username && <DeleteButton postId={id} />} */}
-        <Button as="div" labelPosition="right" onClick={likePost}>
-          <Button color ="teal" basic>
-            <Icon name="heart" />
-          </Button>
-          <Label basic color="teal" poiting="left">
-            {likeCount}
-          </Label>
-        </Button>
-        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          </Button> 
+          </MyPopup>
+         */}
+        <Button  labelPosition="right" as={Link} to={`/posts/${id}`}>
           <Button color ="blue" basic>
             <Icon name="comments" />
           </Button>
@@ -63,6 +60,11 @@ function PostCard({
             {commentCount}
           </Label>
         </Button>
+        {user && user.username === username && (
+          <Button as="div" color="red" floated="right" onClick={() => console.log('Delete post')}>
+            <Icon name="trash" style={{margin: 0}}/>
+          </Button>
+        )} 
       </Card.Content> 
     </Card>
   );
